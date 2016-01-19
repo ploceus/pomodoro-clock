2a4173a53ccefba4f9e85a956f8b8543ddbfe6a2
ploceus <ploceusweb@gmail.com>
2016-01-19 15:54:52 +0100

    Layout movil y ajustes
    Preparar para movil y corregir el reinicio de marcador de tiempo
    transcurrido. No termina de visualizarse correctamente en ipad 1 y algún
    android.

--- main.js
+++ main.js
@@ -6,49 +6,54 @@ $(document).ready(function() {
   $('#sessionLess').click(sessionLess);
   $('#sessionAdd').click(sessionAdd);
   sessionTime();
-  
+
 });
 
-
 //Global Variables
-var contador = 0;// contador = 0 then is the first click and start the timer.
-var pause = 1 ;//0 = running and 1 = paused.
-var restOfTime;//stores the rest of the cicle time.
-var activePhase = 0;// 0 = session active and 1 = break active.
-var phaseTime;//Actual phase length.
-var progress;//Step for fill
-var timerHeight = 300;
+var contador = 0; // contador = 0 then is the first click and start the timer.
+var pause = 1; //0 = running and 1 = paused.
+var restOfTime; //stores the rest of the cicle time.
+var activePhase = 0; // 0 = session active and 1 = break active.
+var phaseTime; //Actual phase length.
+var progress; //Step for fill
 
+var altura = $("#timer").css("height");
+altura = parseInt(altura, 0);
+var timerHeight = altura;
 
 //Functions
 
 //Click the score board.
-function onOff () {
-  if(contador == 0){
+function onOff() {
+  if (contador == 0) {
     sessionTime();
     repeat();
     pause = 0;
     activePhase = 1;
-  }else{
+  } else {
     runAndStop();
   }
   contador++;
 }
 //Time Settings
-var breakLess = function () {
+var breakLess = function() {
+  timerHeight = altura;
+  fillBackground();
   var time = $('#breakTime').text();
   time = parseInt(time);
   if (time > 1) {
     time = time - 1;
-  }else{
+  } else {
     time = 1;
   };
-    
   $('#breakTime').text(time);
   sessionTime();
 }
 
-var breakAdd = function () {
+var breakAdd = function() {
+  timerHeight = altura;
+  fillBackground();
+
   var time = $('#breakTime').text();
   time = parseInt(time);
   time = time + 1;
@@ -56,34 +61,40 @@ var breakAdd = function () {
   sessionTime();
 }
 
-var sessionLess = function () {
+var sessionLess = function() {
+  timerHeight = altura;
+  fillBackground();
   var time = $('#sessionTime').text();
   time = parseInt(time);
   if (time > 1) {
     time = time - 1;
-  }else{
+  } else {
     time = 1;
   };
   $('#sessionTime').text(time);
   $('#time').text(time);
   sessionTime();
+
 }
 
-var sessionAdd = function () {
+var sessionAdd = function() {
+  timerHeight = altura;
+  fillBackground();
   var time = $('#sessionTime').text();
   time = parseInt(time);
   time = time + 1;
   $('#sessionTime').text(time);
   $('#time').text(time);
   sessionTime();
+
 }
 
 //Change of phase (break or session)
-function changeToOther () {
+function changeToOther() {
   if (activePhase == 0) {
     changeToSession();
     activePhase = 1;
-  }else{
+  } else {
     changeToBreak();
     activePhase = 0;
   };
@@ -91,22 +102,21 @@ function changeToOther () {
 }
 
 //Updates the session length
-function sessionTime () {
+function sessionTime() {
   var totalTime = $('#sessionTime').text();
-  totalTime = parseInt(totalTime)*60;
+  totalTime = parseInt(totalTime) * 60;
   restOfTime = totalTime;
   phaseTime = restOfTime;
-  console.log('Duracion de la fase '+phaseTime);
+  console.log('Duracion de la fase ' + phaseTime);
   calculateProgress(phaseTime);
 }
 
-
 //Stop and starting
-function runAndStop () {
-  if(pause == 1){
+function runAndStop() {
+  if (pause == 1) {
     repeat();
     pause = 0;
-  }else{
+  } else {
     clearInterval(intervalRepeat);
     pause = 1;
     //console.log('pause is 1 para');
@@ -114,10 +124,10 @@ function runAndStop () {
 }
 
 //Countdown
-function countdown (){
+function countdown() {
   restOfTime--
   actualizarTime(restOfTime);
-  fillBackground ();
+  fillBackground();
   if (restOfTime == 0) {
     ring();
     clearInterval(intervalRepeat);
@@ -127,14 +137,14 @@ function countdown (){
 }
 
 //Time interval
-function repeat(){
+function repeat() {
   intervalRepeat = setInterval('countdown()', 1000)
 }
 
 //update the value of time in scoreboard
-function actualizarTime (restOfTime) {
-  var minutes = Math.floor(restOfTime/60);
-  minutes = minutes < 10 ? '0' + minutes : minutes;//Necesito repasar ?????????
+function actualizarTime(restOfTime) {
+  var minutes = Math.floor(restOfTime / 60);
+  minutes = minutes < 10 ? '0' + minutes : minutes; //Necesito repasar ?????????
   var seconds = restOfTime % 60;
   seconds = seconds < 10 ? '0' + seconds : seconds;
   var time = minutes + ':' + seconds;
@@ -142,48 +152,48 @@ function actualizarTime (restOfTime) {
 }
 
 //Change to Break.
-function changeToBreak () {
+function changeToBreak() {
   console.log('Change to Break');
   $('#timerTitle').text('BREAK!!!');
+  timerHeight = altura;
   var breakTime = $('#breakTime').text();
-  breakTime = parseInt(breakTime)*60;
+  breakTime = parseInt(breakTime) * 60;
   restOfTime = breakTime;
   phaseTime = restOfTime;
-  console.log('Duracion de la fase '+phaseTime); 
-  calculateProgress (phaseTime);
+  console.log('Duracion de la fase ' + phaseTime);
+  calculateProgress(phaseTime);
+  $('.fillBackground').css('backgroundColor', '#49C79C');
   repeat();
-  timerHeight = 300;
-  $('.fillBackground').css('backgroundColor','#49C79C');
 }
 
 //Change to Session
-function changeToSession () {
+function changeToSession() {
   console.log('Change to Session');
+  timerHeight = altura;
   $('#timerTitle').text('Session');
   sessionTime();
+  $('.fillBackground').css('backgroundColor', '#FF8A00');
   repeat();
-  timerHeight = 300;
-  $('.fillBackground').css('backgroundColor','#FF8A00');
 }
 
 //Alarm phase finished.
-function ring () {
+function ring() {
   var sound = 'http://www.oringz.com/oringz-uploads/bb_sunny.mp3';
   var ringtone = new Audio(sound);
   ringtone.play();
 }
 
 //Fill background timer
-function fillBackground () {
+function fillBackground() {
   timerHeight = timerHeight - progress;
-  var relleno = timerHeight + "px"; 
-  console.log('timerHeight '+ relleno);
-  $('.fillBackground').css('top',relleno);
- }
-  
+  console.log(timerHeight);
+  relleno = timerHeight + "px";
+  $('.fillBackground').css('top', relleno);
+}
+
 //Calculate de progress of the fillBackground.
-function calculateProgress (phaseTime) {
-  progress = 300/phaseTime;
-  progress = progress ;
+function calculateProgress(phaseTime) {
+  progress = timerHeight / phaseTime;
+  // progress = progress ;
   console.log("El progress para la presente sesion es " + progress);
 }
